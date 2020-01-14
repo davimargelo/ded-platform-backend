afa339fad5b6d57c2fa763e5b027c7af87973d4b
Davi <davimargelo@hotmail.com>
2020-01-14 19:43:20 -0300

    First commit / Inicializr

---
+++ src/main/java/com/idh/ded/DedPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.idh.ded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DedPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DedPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idh/ded/DedPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idh.ded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DedPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
