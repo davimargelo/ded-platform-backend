00a6b1f5ae9b09a797bbbdc83e8d3cfd7d5d716c
Iago-Borges <iagobds21@gmail.com>
2020-01-17 15:47:28 -0300

    First Commit for Race

---
+++ src/main/java/com/idh/ded/services/RaceService.java
@@ -0,0 +1,24 @@
+package com.idh.ded.services;
+
+import com.idh.ded.DTOs.Races;
+import com.idh.ded.repositories.RaceRepository;
+import com.idh.ded.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RaceService {
+    @Autowired
+    RaceRepository racesRepository;
+
+    public List<Races> getAll() {
+        return racesRepository.findAll();
+    }
+
+    public Races getOne(String racesId) {
+        return racesRepository.findById(racesId).orElseThrow(() -> new ObjectNotFoundException("404 - Not Found ID: " + racesId, new Throwable("Type: " + Races.class.getName())));
+    }
+
+}
