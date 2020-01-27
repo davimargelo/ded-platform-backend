3764a22d64f66c68ac2750b0c995b72a7d41ca41
Davi <davimargelo@hotmail.com>
2020-01-27 19:35:31 -0300

    D100 ADDED

--- src/main/java/com/idh/ded/domain/enums/DiceType.java
+++ src/main/java/com/idh/ded/domain/enums/DiceType.java
@@ -3,37 +3,38 @@ package com.idh.ded.domain.enums;
 import com.idh.ded.services.exceptions.ObjectNotFoundException;
 
 public enum DiceType {
-        D3(4, "Rolls D4"),
-        D4(6, "Rolls D6"),
-        D8(8, "Rolls D8"),
-        D10(10, "Rolls D10"),
-        D12(12, "Rolls D12"),
-        D20(20, "Rolls D20");
+    D3(4, "Rolls D4"),
+    D4(6, "Rolls D6"),
+    D8(8, "Rolls D8"),
+    D10(10, "Rolls D10"),
+    D12(12, "Rolls D12"),
+    D20(20, "Rolls D20"),
+    D100(100, "Rolls D100");
 
-        private int cod;
-        private String description;
+    private int cod;
+    private String description;
 
-        private DiceType(int cod, String description) {
-            this.cod = cod;
-            this.description = description;
-        }
+    private DiceType(int cod, String description) {
+        this.cod = cod;
+        this.description = description;
+    }
 
-        public int getCod() {
-            return cod;
-        }
+    public int getCod() {
+        return cod;
+    }
 
-        public String getDescription() {
-            return description;
-        }
+    public String getDescription() {
+        return description;
+    }
 
-        public static DiceType toEnum(Integer cod) {
-            if (cod == null)
-                return null;
+    public static DiceType toEnum(Integer cod) {
+        if (cod == null)
+            return null;
 
-            for (DiceType x : DiceType.values())
-                if ((cod == x.getCod()))
-                    return x;
+        for (DiceType x : DiceType.values())
+            if ((cod == x.getCod()))
+                return x;
 
-            throw new ObjectNotFoundException("Invalid dice - number os sides: " + cod);
-        }
+        throw new ObjectNotFoundException("Invalid dice - number os sides: " + cod);
+    }
 }
