9d0b01ead5326d1b825a6de5bcb3cbd286fd8da0
Davi <davimargelo@hotmail.com>
2020-02-09 15:40:19 -0300

    Montar Perfil para gerenciamento de Config (Banco etc)

---
+++ src/main/java/com/idh/ded/configurations/TestConfig.java
@@ -0,0 +1,26 @@
+package com.idh.ded.configurations;
+
+import com.idh.ded.services.DBService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+@Slf4j
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        log.info("Setting up datasource for DEV environment.");
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
